546cb43ec7ae76d83392f8935bea0662f78a1ba1
DanE <edwardsdan001@gmail.com>
2018-03-05 10:46:19 -0500

    Initial commit

---
+++ PantryParty/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PantryParty.Startup))]
+namespace PantryParty
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
