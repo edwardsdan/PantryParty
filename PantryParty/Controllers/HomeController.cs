9c5f25667661dd00078579f444726222946a2372
DanE <edwardsdan001@gmail.com>
2018-03-05 22:10:56 -0500

    Added DisplayRecipes action

--- PantryParty/Controllers/HomeController.cs
+++ PantryParty/Controllers/HomeController.cs
@@ -12,6 +12,9 @@ namespace PantryParty.Controllers
 {
     public class HomeController : Controller
     {
+
+        // Google Directions API Key: AIzaSyCAERkhlLqh6FoMMAa3PFzxn_RZeaYEsXw
+
         public ActionResult Index()
         {
             return View();
@@ -42,49 +45,53 @@ namespace PantryParty.Controllers
 
             //above link is for test/sample only.. use this for actual web: https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/autocomplete?number=10&query={input}
 
-            //the application needs to make the HTTP request 
-            //useragent means which browser is making this request
             request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0";
-
             request.Headers.Add("X-Mashape-Key", "B3lf5QUiIJmshYkZTOsBX2wpV3E2p1RPhROjsnr2jwlt8H1r08");
 
-            //STEP 2. Get response back 
-            //(GetResponse will give us a lot of responses... we're casting here in the '()' to tell it its a specific HTTPWeb response)
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
             if (response.StatusCode == HttpStatusCode.OK) //if everything goes good - ok has the value of 200 ( which = good)
             {
                 StreamReader dataStream = new StreamReader(response.GetResponseStream());
-                //getting data back as a stream - we're getting it back as a stream because it makes it more efficient to read instead of a blob
+                // getting data back as a stream
 
-                string jSonData = dataStream.ReadToEnd(); // read the entire response back (this will include all JSON data)
-
-
-                //parse data (to go)
-                //transform from JSON to HTML
-                JArray JParser = JArray.Parse(jSonData); //this is taking that string and building a JSON tree 
-
-
-                //playing with this.. not working yet
-                ViewBag.Apple = JParser[0]["title"]; //go inside viewbag with for loop
-                ViewBag.Image = JParser[0]["image"];
-                ViewBag.Apple2 = JParser[1]["title"];
-                ViewBag.Image2 = JParser[0]["image"];
-
-
-                return View("ShowResults");
-
-
+                string jSonData = dataStream.ReadToEnd();
+                JArray recipes = JArray.Parse(jSonData); //this is taking that string and building a JSON tree 
+                
+                ViewBag.Data = recipes;
+                return RedirectToAction("DisplayRecipes");
             }
             else // if we have something wrong
             {
                 return View("../Shared/Error");
             }
-
-
-            //200 = all good
-            //4_ _+ (ex:404) = problem on our side
-            //5_ _ = servers fault
         }
 
+        public ActionResult DisplayRecipes(JArray recipes)
+        {
+            try
+            {
+                ViewBag.RecipeInfo = "";
+                foreach (JObject recipe in recipes)
+                {
+                    HttpWebRequest request = WebRequest.CreateHttp("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/" + recipe["id"] + "/information");
+                    request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0";
+                    request.Headers.Add("X-Mashape-Key", "B3lf5QUiIJmshYkZTOsBX2wpV3E2p1RPhROjsnr2jwlt8H1r08");
+
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        StreamReader reader = new StreamReader(response.GetResponseStream());
+                        string output = reader.ReadToEnd();
+                        JObject jParser = JObject.Parse(output);
+                        ViewBag.RecipeInfo += jParser;
+                    }
+                }
+                return View("ShowResults");
+            }
+            catch (Exception)
+            {
+                return View("../Shared/Error");
+            }
+        }
     }
 }
